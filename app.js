56e5a9061964f5eede2768bb1082cc0c5651c379
Rajat <chaurosiarajat@gmail.com>
2023-04-08 18:33:22 +0530

    node basic

---
+++ app.js
@@ -0,0 +1,11 @@
+//  BACKEND DEVELOPER
+
+// Node Js--> its envoirment that run javascript
+  // non blocking i/o operaters
+
+
+  // IIFE--> imidate invoke function expression
+
+//   (function(name){
+//     console.log("vasant fn",name);
+//   })("good boy")
